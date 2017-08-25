27f1caff2241462576eac0929ebe7b3812cfd3eb
sudakov <vsudakov@bk.ru>
2017-08-25 12:07:41 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Заголовок приложения',
+      theme: new ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: new MyHomePage(title: 'Это начальная страница приложения'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _blabla = "";
+  
+  final TextEditingController _controller = new TextEditingController();
+  
+  @override
+  void initState() {
+    super.initState();
+    _readStr().then((String value) {
+      setState(() {
+        _blabla = value;
+        _controller.text = value;
+      });
+    });
+  }
+  
+  Future<File> _getLocalFile() async {
+    // get the path to the document directory.
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    return new File('$dir/blabla.txt');
+  }
+
+  Future<String> _readStr() async {
+    try {
+      File file = await _getLocalFile();
+      // read the variable as a string from the file.
+      String contents = await file.readAsString();
+      return contents;
+    } on FileSystemException {
+      return "Error";
+    }
+  }
+  
+  Future<Null> _setStr() async {
+    setState(() {
+      _blabla = _controller.text;
+    });
+    // write the variable as a string to the file
+    await (await _getLocalFile()).writeAsString('$_blabla');
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug paint" (press "p" in the console where you ran
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+          // window in IntelliJ) to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'Текст в базе',
+            ),
+            new Text(
+              '${_blabla}',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            new TextField(
+              controller: _controller,
+              decoration: new InputDecoration(
+                hintText: 'Type something',
+              ),
+            ),
+            new RaisedButton(
+              onPressed: () {
+                _setStr();
+                showDialog(
+                  context: context,
+                  child: new AlertDialog(
+                    title: new Text('Сохранен текст:'),
+                    content: new Text(_controller.text),
+                  ),
+                );
+              },
+              child: new Text('Сохранить'),
+            ),
+          ],
+        ),
+      ),
+     
+    );
+  }
+}
