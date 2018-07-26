16306216a5299ee9112e8c1493f099b92792ac28
sudakov <vsudakov@bk.ru>
2018-07-26 12:29:18 +0300

    sentry

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,21 @@ import 'dart:io';
 
 import 'package:diod/app/app.dart';
 import 'package:diod/config/app_config.dart';
+import 'package:diod/config/secret_loader.dart';
+import 'package:diod/config/secret.dart';
 
-void main() {
+void main() async {
   bool development = false;
   assert(development = true); // Метод выполняется только в debug режиме
   String developmentUrl = Platform.isIOS ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
-
+  Secret secret;
+  String sentryDsn = "";
+  secret = await SecretLoader(secretPath: "lib/data/secrets.json").load();
+  sentryDsn = secret.sentryDsn;
   App.setup(AppConfig(
     env: development ? 'development' : 'production',
     databaseVersion: 1,
     apiBaseUrl: '${development ? developmentUrl : 'https://rapi.unact.ru'}/api/',
-    sentryDsn: ''
+    sentryDsn: sentryDsn
   )).run();
 }
