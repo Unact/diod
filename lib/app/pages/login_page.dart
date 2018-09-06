e649c27ab5bb2baeab5d97949577a394ebbdf152
sudakov <vsudakov@bk.ru>
2018-09-06 16:31:53 +0300

    to old iPhone

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -85,7 +85,7 @@ class _LoginPageState extends State<LoginPage> {
       body: new ListView(
         children: <Widget>[
           new Container(
-              padding: const EdgeInsets.only(top: 180.0, left: 16.0, right: 16.0),
+              padding: const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
               child: new Form(
                   key: _formKey,
                   child: new Column(
@@ -122,7 +122,7 @@ class _LoginPageState extends State<LoginPage> {
                           new Padding(
                             padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
                             child: new Container(
-                              width: 120.0,
+                              width: 100.0,
                               child: new RaisedButton(
                                 onPressed: _submit,
                                 color: Colors.blueAccent,
@@ -134,7 +134,7 @@ class _LoginPageState extends State<LoginPage> {
                           new Padding(
                               padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
                               child: new Container(
-                                width: 160.0,
+                                width: 150.0,
                                 child: new RaisedButton(
                                   onPressed: _getNewPassword,
                                   color: Colors.blueAccent,
